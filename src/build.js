4b254c83486a1d5d1b21d5e0812f8082b56e3313
mehmetcansahin <sahinmcan@gmail.com>
2020-05-20 15:21:16 +0300

    build bundle

--- src/build.js
+++ src/build.js
@@ -2,10 +2,11 @@ import Vue from "vue";
 import VueCrudTable from "./components/TableComponent.vue";
 
 const Components = {
-  VueCrudTable
-}
-Object.keys(Components).forEach(name => {
-  	Vue.component(name, require(Components[name]).default);
+  VueCrudTable,
+};
+
+Object.keys(Components).forEach((name) => {
+  Vue.component(name, require(Components[name]).default);
 });
 
 export default Components;
