9a3f3b25d982e91d27a03323db8e714714829f4f
mehmetcansahin <sahinmcan@gmail.com>
2020-05-20 03:59:41 +0300

    update

--- src/components/form/RadioComponent.vue
+++ src/components/form/RadioComponent.vue
@@ -1,12 +1,11 @@
 <template>
-<div>
+  <div>
     <label class="label">{{ field.title }}</label>
-    <div class="control" v-for="(item, index) in field.values">
-     	<label class="radio">
-    		<input type="radio" :name="field.name" v-model="data[field.name]" :value="item"/>
-   			{{item}}
-  		</label>
-     	
+    <div class="control" v-for="(item, index) in field.values" v-bind:key="index">
+      <label class="radio">
+        <input type="radio" :name="field.name" v-model="data[field.name]" :value="item" />
+        {{item}}
+      </label>
     </div>
     <p class="help" v-if="field.note">{{ field.note }}</p>
   </div>
@@ -14,7 +13,7 @@
 
 <script>
 export default {
-  props: ["field", "data"],
+  props: ["field", "data"]
 };
 </script>
 
